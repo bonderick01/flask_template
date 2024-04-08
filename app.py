47779e4941246eeece52a5612784937e7b453469
Sam Starnes <samual-starnes@uiowa.edu>
2024-04-08 15:22:41 -0500

    Second driver sequence done without picture

--- app.py
+++ app.py
@@ -7,9 +7,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/mike')
+@app.route('/about')
 def mike():
-    return render_template('mike.html')
+    return render_template('about.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
